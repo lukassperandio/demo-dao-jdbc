4e2a1827995c266af8b33dce95bcba4da5ef130c
Lukas Sperandio <emanuelsperandio@gmail.com>
2024-02-04 12:08:47 +0000

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,15 @@ public class Program {
 			System.out.println(obj);
 		}
 
+		System.out.println("\n=== TEST 3: seller findByAll ===");
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
+
+
+
